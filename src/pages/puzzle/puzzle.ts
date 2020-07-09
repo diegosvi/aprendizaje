9e8f1813df414648ed1a96c907e9e19813d9971f
pilar1304 <pilar1304@hotmail.es>
2020-07-09 18:56:27 -0500

    #13 Cambios fondo colores y boton play

--- src/pages/puzzle/puzzle.ts
+++ src/pages/puzzle/puzzle.ts
@@ -98,35 +98,22 @@ export class PuzzlePage {
 
      openImagePicker(){
       this.options = {
-        // Android only. Max images to be selected, defaults to 15. If this is set to 1, upon
-        // selection of a single image, the plugin will return it.
-        //maximumImagesCount: 3,
-  
-        // max width and height to allow the images to be.  Will keep aspect
-        // ratio no matter what.  So if both are 800, the returned image
-        // will be at most 800 pixels wide and 800 pixels tall.  If the width is
-        // 800 and height 0 the image will be 800 pixels wide if the source
-        // is at least that wide.
         width: 200,
-        //height: 200,
-  
-        // quality of resized image, defaults to 100
         quality: 25,
-  
-        // output type, defaults to FILE_URIs.
-        // available options are 
-        // window.imagePicker.OutputType.FILE_URI (0) or 
-        // window.imagePicker.OutputType.BASE64_STRING (1)
         outputType: 1
       };
       this.imageResponse = [];
       this.imagePicker.getPictures(this.options).then((results) => {
         for (var i = 0; i < results.length; i++) {
           //this.imageResponse.push('data:image/jpeg;base64,' + results[i]);
-          this.imgTemp=this.imageResponse.push('data:image/jpeg;base64,' + results[i]);
+          //this.imgTemp=this.imageResponse.push('data:image/jpeg;base64,' + results[i]);
           /*this.imageUrl = this.imgTemp;*/
-          //alert(results);
-          this.imageUrl = results;
+          alert(results);
+          /*this.imageUrl = results;*/
+
+          console.log('Image URI: ' + normalizeURL(results[i]));
+          this.imgTemp = normalizeURL(results[i]);
+          alert(this.imgTemp);
         }
       }, (err) => {
         alert(err);
